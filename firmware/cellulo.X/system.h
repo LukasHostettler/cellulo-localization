1c479d654a7278db3d18f87041a5cda34c72ba32
Ayberk Özgür <ayberk.ozgur@epfl.ch>
2015-04-23 08:48:17 +0200

    Removed system and user

--- firmware/cellulo.X/system.h
+++
@@ -1,7 +0,0 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
-
-#define SYS_FREQ     80000000L
-#define FCY          SYS_FREQ
-
-#endif
--- firmware/cellulo.X/user.h
+++
@@ -1,6 +0,0 @@
-#ifndef USER_H
-#define USER_H
-
-void InitApp(void);         /* I/O and Peripheral Initialization */
-
-#endif
