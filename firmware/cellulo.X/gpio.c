a303c03952728b1fb449813c601f25a889f77ac2
Ayberk Özgür <ayberk.ozgur@epfl.ch>
2015-04-23 15:15:56 +0200

    GPIO setup

--- firmware/cellulo.X/gpio.c
+++ firmware/cellulo.X/gpio.c
@@ -1,6 +1,6 @@
 /**
  * @file gpio.h
- * @brief Source for gpio pin functionality
+ * @brief Source for GPIO pin functionality
  * @author Ayberk Özgür
  * @version 1.0
  * @date 2015-04-23
@@ -9,33 +9,65 @@
 #include"gpio.h"
 
 void setupGPIO(){
+    
     //BAT_PG input
+    TRISBbits.TRISB5 = 1;   //Set as input
+    ANSELBbits.ANSB5 = 0;   //Digital function
 
     //BAT_STAT2 input
+    TRISBbits.TRISB4 = 1;   //Set as input
+    ANSELBbits.ANSB4 = 0;   //Digital function
 
     //BAT_STAT1/LBO input
+    TRISBbits.TRISB3 = 1;   //Set as input
+    ANSELBbits.ANSB3 = 0;   //Digital function
 
     //VBAT_SENSE_ON output
+    TRISBbits.TRISB12 = 0;  //Set as output
+    ANSELBbits.ANSB12 = 0;  //Digital function
+    VBAT_SENSE_ON = 0;      //Turn off for now
 
     //BAT_CE output
+    TRISBbits.TRISB13 = 0;  //Set as output
+    ANSELBbits.ANSB13 = 0;  //Digital function
+    BAT_CE = 0;             //Turn off for now
 
     //BT_RESET_BAR output
+    TRISCbits.TRISC12 = 0;  //Set as output
+    BT_RESET_BAR = 1;       //Do not reset for now
 
     //BT_BAUD_9600 output
+    TRISCbits.TRISC15 = 0;  //Set as output
+    BT_BAUD_9600 = 1;       //Set baud rate to 9600 for now
 
     //TOUCH_RESET_BAR output
+    TRISFbits.TRISF3 = 0;   //Set as output
+    TOUCH_RESET_BAR = 1;    //Do not reset for now
 
     //TOUCH_CHANGE (interrupt on change) input
+    TRISDbits.TRISD9 = 1;   //Set as input
+                            //TODO: Setup interrupt on change
 
     //IMG_OE output
+    TRISDbits.TRISD0 = 0;   //Set as output
+    IMG_OE = 0;             //Turn off for now
 
     //LED_XLAT output
+    TRISCbits.TRISC14 = 0;  //Set as output
+    LED_XLAT = 0;           //Do not rise for now
 
     //IMG_RESET_BAR output
+    TRISDbits.TRISD2 = 0;   //Set as output
+    IMG_RESET_BAR = 1;      //Do not reset for now
 
     //IMG_EXPOSURE output
+    TRISDbits.TRISD3 = 0;   //Set as output
+    IMG_EXPOSURE = 0;       //Turn off for now
 
     //IMG_STANDBY output
+    TRISFbits.TRISF0 = 0;   //Set as output
+    IMG_STANDBY = 0;        //Turn off for now
 
     //PIXEL_LINE (interrupt on change) input
+    TRISFbits.TRISF1 = 1;   //Set as input
 }
--- firmware/cellulo.X/gpio.h
+++ firmware/cellulo.X/gpio.h
@@ -1,6 +1,6 @@
 /**
  * @file gpio.h
- * @brief Header for gpio pin functionality
+ * @brief Header for GPIO pin functionality
  * @author Ayberk Özgür
  * @version 1.0
  * @date 2015-04-23
@@ -12,6 +12,22 @@
 #ifdef __XC32
     #include <xc.h>
 #endif
+
+#define BAT_PG          PORTBbits.RB5
+#define BAT_STAT2       PORTBbits.RB4
+#define BAT_STAT1_LBO   PORTBbits.RB3
+#define VBAT_SENSE_ON   PORTBbits.RB12
+#define BAT_CE          PORTBbits.RB13
+#define BT_RESET_BAR    PORTCbits.RC12
+#define BT_BAUD_9600    PORTCbits.RC15
+#define TOUCH_RESET_BAR PORTFbits.RF3
+#define TOUCH_CHANGE    PORTDbits.RD9
+#define IMG_OE          PORTDbits.RD0
+#define LED_XLAT        PORTCbits.RC14
+#define IMG_RESET_BAR   PORTDbits.RD2
+#define IMG_EXPOSURE    PORTDbits.RD3
+#define IMG_STANDBY     PORTFbits.RF0
+#define PIXEL_LINE      PORTFbits.RF1
 
 /**
  * @brief Sets up GPIO pins
